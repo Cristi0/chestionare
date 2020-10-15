9227445d1f59f623efa7a25a7209b8c0447de8c4
Cristi <p.cristi0@yahoo.ro>
2020-10-15 23:33:36 +0300

    Initial commit

---
+++ src/main/java/com/management/chestionare/Configuration/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.management.chestionare.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+}
---
+++ src/main/java/com/management/chestionare/ManagementChestionareApplication.java
@@ -0,0 +1,13 @@
+package com.management.chestionare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementChestionareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementChestionareApplication.class, args);
+    }
+
+}
